1104987bc320fe6de2341a176319c769fc82ba04
minseunk <minseunk@c8r5s3.42seoul.kr>
2023-04-16 21:29:31 +0900

    str

--- pf/print_str.c
+++ pf/print_str.c
@@ -6,11 +6,31 @@
 /*   By: minseunk <minseunk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 19:18:11 by minseunk          #+#    #+#             */
-/*   Updated: 2023/04/16 20:46:52 by minseunk         ###   ########.fr       */
+/*   Updated: 2023/04/16 21:27:09 by minseunk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	putnull(t_format form, int *cnt)
+{
+	int		flag;
+	int		i;
+	char	*null;
+
+	flag = 0;
+	null = "(null)";
+	i = -1;
+	if (form.prec < 0 || form.prec > 6)
+		form.prec = 6;
+	while (++i < form.prec)
+	{
+		flag = putchar_proc_error(null[i], cnt);
+		if (flag == -1)
+			return (-1);
+	}
+	return (0);
+}
 
 int	putstr_proc_error(char *str, t_format form, int *cnt)
 {
@@ -23,29 +43,14 @@ int	putstr_proc_error(char *str, t_format form, int *cnt)
 		len = form.prec;
 	else
 		len = ft_strlen(str);
+	if (str == NULL)
+	{
+		if (putnull(form, cnt))
+			return (-1);
+	}
 	while (i < len && str[i])
 	{	
 		flag = putchar_proc_error(str[i++], cnt);
-		if (flag == -1)
-			return (-1);
-	}
-	return (0);
-}
-
-int	putnull(t_format form, int *cnt)
-{
-	int		flag;
-	int		i;
-	char	*null;
-
-	flag = 0;
-	null = "(null)";
-	i = 0;
-	if (form.prec < 0)
-		form.prec = 6;
-	while (i < form.prec)
-	{
-		flag = putchar_proc_error(null[i++], cnt);
 		if (flag == -1)
 			return (-1);
 	}
@@ -60,22 +65,21 @@ int	get_spwi(t_format form, va_list *ap)
 
 	va_copy(cp, *ap);
 	str = va_arg(cp, char *);
-	if (form.prec >= 0 && form.prec < form.widt)
+	strlen = 0;
+	if (form.prec >= 0 && form.prec < ft_strlen(str))
 		strlen = form.prec;
 	else
 		strlen = ft_strlen(str);
+	if (str == NULL)
+		strlen = 6 - (form.prec * (form.prec >= 0 && form.prec < 6));
 	return (form.widt - strlen);
 }
 
 int	print_str(t_format form, va_list *ap, int *cnt)
 {
-	va_list	cp;
 	int		sp_width;
 
 	sp_width = get_spwi(form, ap);
-	va_copy(cp, *ap);
-	if (va_arg(cp, char *) == NULL)
-		return (putnull(form, cnt));
 	if (form.flag & MNS)
 	{
 		if (putstr_proc_error(va_arg(*ap, char *), form, cnt) == -1)
--- test.c
+++ test.c
@@ -4,6 +4,6 @@ int ft_printf(char *str, ...);
 
 int main()
 {
-	printf("%d\n",printf(" %.2s %.3s %.4s %.5s %.1s ", " - ", "", "4", "", "2 "));
-	printf("%d\n",ft_printf(" %.2s %.3s %.4s %.5s %.1s ", " - ", "", "4", "", "2 "));
+	printf("%d\n",printf("%32s", NULL));
+	printf("%d\n",ft_printf("%32s", NULL));
 }
