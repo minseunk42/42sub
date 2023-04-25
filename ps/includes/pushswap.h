f3fe375c61abbb4a07d937bc2e188e7754155cf3
minseunk <minseunk@c3r4s7.42seoul.kr>
2023-04-25 21:19:39 +0900

    lstTest

--- ps/includes/pushswap.h
+++ ps/includes/pushswap.h
@@ -1,1 +1,30 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pushswap.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minseunk <minseunk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/25 20:39:19 by minseunk          #+#    #+#             */
+/*   Updated: 2023/04/25 21:03:56 by minseunk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSHSWAP_H
+# define PUSHSWAP_H
+
+# include <unistd.h>
+
+typedef struct s_node
+{
+	int				num;
+	struct s_node	*next;
+}	t_node;
+
+typedef struct s_stack
+{
+	struct s_node	*head;
+	struct s_node	*tail;
+}	t_stack;
+
+#endif
--- ps/srcs/main.c
+++ ps/srcs/main.c
@@ -1,8 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minseunk <minseunk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/25 20:39:10 by minseunk          #+#    #+#             */
+/*   Updated: 2023/04/25 21:18:59 by minseunk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pushswap.h"
+#include <stdlib.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (0);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
+
+
+
+t_list	*ft_lstnew(void *content);
 int	ft_printf(const char *str, ...);
 
 
-
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	proc_arg(ac, av);
+	t_list *lst;
+
+	(void)av;
+	(void)ac;
+	int *tmp = (int *)malloc(sizeof(int));
+	 
+	*tmp = 3;
+	lst = ft_lstnew(tmp);
+	ft_printf("%d", *(int *)lst->content);
 }
