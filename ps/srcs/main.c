45c6c14e74e7f5d32ab9030785c7357f38e1c71f
minseunk <minseunk42@gmail.com>
2023-04-26 09:45:31 +0000

    md makefile order

--- ps/srcs/main.c
+++ ps/srcs/main.c
@@ -3,21 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minseunk <minseunk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 20:39:10 by minseunk          #+#    #+#             */
-/*   Updated: 2023/04/25 21:18:59 by minseunk         ###   ########.fr       */
+/*   Updated: 2023/04/26 09:31:17 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pushswap.h"
 #include <stdlib.h>
-
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }	t_list;
+
+t_list	*ft_lstnew(void *content);
+int	ft_printf(const char *str, ...);
+
+
 
 t_list	*ft_lstnew(void *content)
 {
@@ -33,8 +37,6 @@ t_list	*ft_lstnew(void *content)
 
 
 
-t_list	*ft_lstnew(void *content);
-int	ft_printf(const char *str, ...);
 
 
 int	main(int ac, char **av)
