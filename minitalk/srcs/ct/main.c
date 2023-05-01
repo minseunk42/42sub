9b0fb299167de9bc8fce6a4142f68076757ac60a
minseunk <minseunk42@gmail.com>
2023-05-01 21:25:26 +0000

    mt_1st_try

--- minitalk/srcs/ct/main.c
+++ minitalk/srcs/ct/main.c
@@ -6,11 +6,26 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 19:32:32 by minseunk          #+#    #+#             */
-/*   Updated: 2023/04/29 13:32:58 by ubuntu           ###   ########.fr       */
+/*   Updated: 2023/05/01 21:24:00 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minitalk.h"
+
+int	g_flag;
+
+void	sig_hander(int sig)
+{
+	if (sig == SIGUSR1)
+	{
+		g_flag = 0;
+		write(1, "sucess\n", 7);
+	}
+	if (sig == SIGUSR2)
+		g_flag++;
+	if (g_flag == 1)
+		write(1, "fail\n", 5);
+}
 
 int	check_pid(char *str, int *pid)
 {
@@ -26,17 +41,43 @@ int	check_pid(char *str, int *pid)
 	return (0);
 }
 
+void	send_sig(char c, int svpid)
+{
+	int	offset;
+	
+	offset = 8;
+	if (g_flag)
+		return ;
+	while (--offset >= 0)
+	{
+		usleep(100);
+		if (1 & (c >> offset))
+			kill(svpid, SIGUSR1);
+		else
+			kill(svpid, SIGUSR2);
+	}
+}
+
 int	main(int ac, char **av)
 {
-	int	svpid;
-	///
-
+	int					svpid;
+	int					i;
+	struct sigaction	act;
+	
 	svpid = 0;
 	if (ac != 3 || check_pid(av[1], &svpid))
+	{
+		write(2, "input arg error\n", 16);
 		return (-1);
-	if (av[2][0] == '2')
-		kill(svpid, SIGUSR2);
-	else
-		kill(svpid, SIGUSR1);
+	}
+	sigemptyset(&act.sa_mask);
+	act.sa_handler = sig_hander;
+	act.sa_flags = SA_RESTART;
+	sigaction(SIGUSR1, &act, 0);
+	sigaction(SIGUSR2, &act, 0);
+	i = -1;
+	while (av[2][++i])
+		send_sig(av[2][i], svpid);
+	send_sig(0, svpid);
 	return (0);
 }
--- minitalk/srcs/svr/main.c
+++ minitalk/srcs/svr/main.c
@@ -3,39 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minseunk <minseunk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 19:32:28 by minseunk          #+#    #+#             */
-/*   Updated: 2023/05/01 20:53:17 by minseunk         ###   ########.fr       */
+/*   Updated: 2023/05/01 21:22:26 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minitalk.h"
 
-extern char	*g_str;
+int	g_flag;
 
-void	sig_hander(int sig)
+void	sig_hander(int sig, siginfo_t *info, void *context)
 {
-	static char	idx;
+	static int	offset;
 	static char	temp;
 
-	(void)sig;
-	idx = 7;
-	temp = 0;
-	ft_printf("hi");
+	(void)context;
+	if (!g_flag)
+		g_flag = info->si_pid;
+	if (g_flag != info->si_pid)
+	{
+		kill(info->si_pid, SIGUSR2);
+		return ;
+	}
+	if (sig == SIGUSR1)
+		temp |= (1 << (7 - offset));
+	offset++;
+	if (offset == 8)
+	{
+		if (!temp)
+		{
+			kill(info->si_pid, SIGUSR1);
+			g_flag = 0;
+		}
+		write(1, &temp, 1);
+		offset = 0;
+		temp = 0;
+	}
 }
 
 int	main(void)
 {
 	struct sigaction	act;
 
-	act.sa_handler = sig_hander;
-	act.sa_mask = 0;
-	act.sa_flags = (SA_RESTART | SA_SIGINFO);
-	ft_printf("%d", getpid());
+	sigemptyset(&act.sa_mask);
+	sigaddset(&act.sa_mask, SIGUSR1);
+	sigaddset(&act.sa_mask, SIGUSR2);
+	act.sa_flags = SA_SIGINFO | SA_RESTART;
+	act.sa_sigaction = sig_hander;
 	sigaction(SIGUSR1, &act, 0);
+	sigaction(SIGUSR2, &act, 0);
+	ft_printf("%d\n", getpid());
 	while (1)
-	{
 		pause();
-	}
 }
