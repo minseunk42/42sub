7818867ef232a4d610a2ad01676a49a774718889
minseunk <minseunk@c3r7s1.42seoul.kr>
2023-05-01 21:10:49 +0900

    action

--- minitalk/srcs/svr/main.c
+++ minitalk/srcs/svr/main.c
@@ -6,7 +6,7 @@
 /*   By: minseunk <minseunk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 19:32:28 by minseunk          #+#    #+#             */
-/*   Updated: 2023/04/27 20:57:20 by minseunk         ###   ########.fr       */
+/*   Updated: 2023/05/01 20:53:17 by minseunk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,28 @@
 
 extern char	*g_str;
 
-void	sig_handler0(int sig)
+void	sig_hander(int sig)
 {
-	ft_printf("sig1%d",sig);
-}
+	static char	idx;
+	static char	temp;
 
-void	sig_handler1(int sig)
-{
-	ft_printf("sig2%d",sig);
+	(void)sig;
+	idx = 7;
+	temp = 0;
+	ft_printf("hi");
 }
 
 int	main(void)
 {
-	void	(*sig_handler[2])(int);
+	struct sigaction	act;
 
-	sig_handler[0] = sig_handler0;
-	sig_handler[1] = sig_handler1;
+	act.sa_handler = sig_hander;
+	act.sa_mask = 0;
+	act.sa_flags = (SA_RESTART | SA_SIGINFO);
 	ft_printf("%d", getpid());
+	sigaction(SIGUSR1, &act, 0);
 	while (1)
 	{
-		signal(SIGUSR1, sig_handler[0]);
-		signal(SIGUSR2, sig_handler[1]);
-		usleep(1000000);
+		pause();
 	}
 }
