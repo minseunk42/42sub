a016fecf31505167343c7f61c1cec927c871dfa6
minseunk <minseunk42@gmail.com>
2023-05-26 20:30:01 +0000

    ps_fin

--- ps2/srcs/ps_main.c
+++ ps2/srcs/ps_main.c
@@ -6,7 +6,7 @@
 /*   By: minseunk <minseunk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 19:05:51 by minseunk          #+#    #+#             */
-/*   Updated: 2023/05/26 18:12:55 by minseunk         ###   ########.fr       */
+/*   Updated: 2023/05/26 20:17:53 by minseunk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ char	*btoa(char *answer, t_stack st_arr[])
 {
 	while (st_arr[STB].size)
 	{
+	
 		if (st_arr[STB].size / 2 > get_maxidx(st_arr))
 		{
 			while (st_arr[STB].head->val[ORDER] != st_arr[STB].size -1)
@@ -53,13 +54,15 @@ char	*btoa(char *answer, t_stack st_arr[])
 	return (answer);
 }
 
-char	*atob(char *answer, t_stack st_arr[], int chunk)
+char	*atob(char *answer, t_stack st_arr[])
 {
 	int	i;
+	int	chunk;
 
 	i = 0;
 	while (st_arr[STA].size)
 	{
+		chunk = (st_arr[STA].size / 100) * 8 + 7;
 		if (st_arr[STA].head->val[ORDER] <= i)
 		{
 			i++;
@@ -79,7 +82,6 @@ char	*atob(char *answer, t_stack st_arr[], int chunk)
 			answer = ft_strjoin(answer, CRA);
 			proc_cmd(CRA, st_arr);
 		}
-		chunk = (st_arr[STA].size / 100) * 10 + 7;
 	}
 	return (answer);
 }
@@ -87,10 +89,14 @@ char	*atob(char *answer, t_stack st_arr[], int chunk)
 char	*algo(char *answer, t_stack st_arr[])
 {
 	t_stack *temp;
-
+	t_stack *temp2;
+	
 	temp = (t_stack *)ft_calloc(sizeof(t_stack), 3);
 	ft_memcpy(temp, st_arr, 3 * sizeof(t_stack));
-	answer = atob(answer, temp, (st_arr[STA].size / 100) * 10 + 7);
+	temp2 = (t_stack *)ft_calloc(sizeof(t_stack), 3);
+	ft_memcpy(temp2, st_arr, 3 * sizeof(t_stack));
+	answer = atob(answer, temp);
+	answer = opti(answer, temp2);
 	answer = btoa(answer, temp);
 	free(temp);
 	return (answer);
---
+++ ps2/test.sh
@@ -0,0 +1,27 @@
+CNT=500
+LIMIT=5500
+INDEX=0
+ITER=100
+NUM_OVER=0
+MAX=$LIMIT
+ARG=`jot -r $CNT -15000000 15000000 | tr "\n" " "`
+NUM=`./push_swap $ARG | wc -l`
+ 
+while [ $INDEX -lt $ITER ]
+do
+echo "$INDEX    :    $NUM"
+if [ $NUM -ge $LIMIT ]; then
+NUM_OVER=`expr $NUM_OVER + 1`
+fi
+if [ $NUM -gt $MAX ]; then
+MAX=$NUM
+fi
+ARG=`jot -r $CNT -15000000 15000000 | tr "\n" " "`
+NUM=`./push_swap $ARG | wc -l`
+INDEX=`expr $INDEX + 1`
+done
+echo "$NUM_OVER overred operations for $ITER times"
+if [ $NUM_OVER -gt 0 ]; then
+echo "Biggest operation number : $MAX"
+echo "fix it!!!!!!!!!!"
+fi
