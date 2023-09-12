7f06352eb0cdb48c5a1f12f1b28c688d115ae111
minseunk <minseunk@c2r3s2.42seoul.kr>
2023-09-12 20:31:24 +0900

    quick

---
+++ philo/header/philo.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minseunk <minseunk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 19:57:16 by minseunk          #+#    #+#             */
+/*   Updated: 2023/09/12 20:31:05 by minseunk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <sys/time.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+
+typedef struct s_arg
+{
+	struct timeval	it;
+	int				nplo;
+	int				teat;
+	int				tdie;
+	int				tslp;
+	int				meat;
+}	t_arg;
+
+int	atoi(const char *str);
+
+#endif
