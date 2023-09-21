75c9d8e9768e178aeca1b4a08e1b6ef342b9a6c6
Ubuntu <ubuntu@ip-172-31-13-17.ap-northeast-2.compute.internal>
2023-09-21 23:22:39 +0000

    add_fork.c ;

---
+++ philo/srcs/fork.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fork.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 19:50:16 by ubuntu            #+#    #+#             */
+/*   Updated: 2023/09/21 21:00:09 by ubuntu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	is_fin(t_philo *philo)
+{
+	pthread_mutex_lock(&(philo->arg->mutex));
+	if (philo->arg->isfin)
+	{
+		pthread_mutex_unlock(&(philo->arg->mutex));
+		return (-1);
+	}
+	pthread_mutex_unlock(&(philo->arg->mutex));
+	if ((int)((get_usec() - philo->ltteat) / 1000) > philo->arg->ttdie)
+	{
+		pthread_mutex_lock(&(philo->arg->mutex));
+		printf("%lu %d died\n" \
+			 , (get_usec() - philo->arg->itime) / 1000, philo->philon);
+		philo->arg->isfin = 1;		
+		pthread_mutex_unlock(&(philo->arg->mutex));
+		return (-1);
+	}
+	if (philo->eatcnt == philo->arg->mxeat)
+	{
+		pthread_mutex_lock(&(philo->arg->mutex));
+		philo->arg->isfin = 1;		
+		pthread_mutex_unlock(&(philo->arg->mutex));
+		return (-1);
+	}
+	return (0);
+}
+
+int	take_rfork(t_philo *philo)
+{
+	int	fnum;
+
+	fnum = (philo->philon + 1) % philo->arg->nofpl;
+	while (1)
+	{
+		if (is_fin(philo))
+			return (-1);
+		pthread_mutex_lock(&(philo->arg->fmtx[fnum]));
+		if (!(philo->arg->fork[fnum]))
+		{
+			printf("%lu %d has taken a fork\n" \
+			 , (get_usec() - philo->arg->itime) / 1000,\
+				 philo->philon);
+			philo->arg->fork[fnum] = 1;
+			break ;
+		}
+		pthread_mutex_unlock(&(philo->arg->fmtx[fnum]));
+		usleep(2);
+	}
+	pthread_mutex_unlock(&(philo->arg->fmtx[fnum]));
+	return (0);
+}
+
+int	take_lfork(t_philo *philo)
+{
+	while (1)
+	{
+		if (is_fin(philo))
+			return (-1);
+		pthread_mutex_lock(&(philo->arg->fmtx[philo->philon]));
+		if (!(philo->arg->fork[philo->philon]))
+		{
+			printf("%lu %d has taken a fork\n" \
+			 , (get_usec() - philo->arg->itime) / 1000,\
+				 philo->philon);
+			philo->arg->fork[philo->philon] = 1;
+			break ;
+		}
+		pthread_mutex_unlock(&(philo->arg->fmtx[philo->philon]));
+		usleep(2);
+	}
+	pthread_mutex_unlock(&(philo->arg->fmtx[philo->philon]));
+	return (0);
+}
+
+int	backfork(t_philo *philo)
+{
+	struct timeval	now;
+
+	gettimeofday(&now, NULL);
+	pthread_mutex_lock(&(philo->arg->mutex));
+	philo->arg->fork[(philo->philon + 1) % philo->arg->nofpl] = 0;
+	philo->arg->fork[philo->philon] = 0;
+	pthread_mutex_unlock(&(philo->arg->mutex));
+	return ((get_usec() - (now.tv_sec * 1000000 + now.tv_usec)) / 1000);
+}
