9081d66437414af244966a9abb8a29a68bd254a6
minseunk <minseunk@c2r7s5.42seoul.kr>
2023-10-08 19:45:21 +0900

    fix_fdf_init()

--- fdf/srcs/main.c
+++ fdf/srcs/main.c
@@ -6,11 +6,38 @@
 /*   By: minseunk <minseunk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 11:38:54 by minseunk          #+#    #+#             */
-/*   Updated: 2023/07/02 11:44:47 by minseunk         ###   ########.fr       */
+/*   Updated: 2023/10/08 19:44:28 by minseunk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
+
+int	is_fdf_file(char *file)
+{
+	int	i;
+
+	i = ft_strlen(file);
+	if (file[--i] && file[i] != 'f')
+		return (-1);
+	if (file[--i] && file[i] != 'd')
+		return (-1);
+	if (file[--i] && file[i] != 'f')
+		return (-1);
+	if (file[--i] && file[i] != '.')
+		return (-1);
+	return (0);
+}
+
+int	init(t_fdf *fdf, char *file)
+{
+	if (is_fdf_file(file))
+		return (-1);
+	if (map_init(fdf, file))
+		return (-1);
+	fdf->mlx_ptr = mlx_init();
+	fdf->win_ptr = mlx_new_window(fdf->mlx_ptr, COLPIX, ROWPIX, "fdf");
+	return (0);
+}
 
 int	fin(void *param)
 {
