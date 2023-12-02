6d84fc65c9ea2e225d6ad080c679192d7a1cce40
minseunk <minseunk@c2r1s4.42seoul.kr>
2023-12-02 17:39:41 +0900

    fout_test

--- cpp/cpp01/ex04/main.cpp
+++ cpp/cpp01/ex04/main.cpp
@@ -1,7 +1,13 @@
 #include <fstream>
+#include <iostream>
 #include <string>
 
 int main(int ac, char **av)
 {
-
+    std::string out_name;
+    out_name += av[1];
+    out_name += ".replace";
+    std::ofstream fout(out_name, std::ios_base::out);
+    fout << "123" << std::endl;
+    fout.close();
 }
