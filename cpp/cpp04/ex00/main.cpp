da87e3065dad234a98f3eacb401aecd93748a20d
minseunk <minseunk@c2r1s4.42seoul.kr>
2023-12-31 06:37:12 +0900

    testcase

--- cpp/cpp04/ex00/main.cpp
+++ cpp/cpp04/ex00/main.cpp
@@ -4,33 +4,39 @@
 #include "WrongAnimal.hpp"
 #include "WrongCat.hpp"
 
-int main()
+int main(int ac, char **av)
 {
-    const Animal* meta = new Animal();
-    const Animal* j = new Dog();
-    Animal* i = new Cat();
-    std::cout << meta->getType() << " " << std::endl;
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    meta->makeSound();
-    i->makeSound(); //will output the cat sound!
-    j->makeSound();
+    if (ac != 2)
+        return -1;
+    if (std::string(av[1]) == "1")
+    {
+        const Animal* meta = new Animal();
+        const Animal* j = new Dog();
+        Animal* i = new Cat();
+        std::cout << meta->getType() << " " << std::endl;
+        std::cout << j->getType() << " " << std::endl;
+        std::cout << i->getType() << " " << std::endl;
+        meta->makeSound();
+        i->makeSound(); //will output the cat sound!
+        j->makeSound();
 
-    delete meta;
-    delete j;
-    delete i;
+        delete meta;
+        delete j;
+        delete i;
+    }
+    else
+    {
+        std::cout << "-----wrong test-----" << std::endl;
 
-    std::cout << "-----wrong test-----" << std::endl;
+        const WrongAnimal* wmeta = new WrongAnimal();
+        WrongAnimal* wi = new WrongCat();
+        std::cout << wmeta->getType() << " " << std::endl;
+        std::cout << wi->getType() << " " << std::endl;
+        wi->makeSound(); //will output the wrong_cat sound!
+        wmeta->makeSound();
 
-    const WrongAnimal* wmeta = new WrongAnimal();
-    WrongAnimal* wi = new WrongCat();
-    std::cout << wmeta->getType() << " " << std::endl;
-    std::cout << wi->getType() << " " << std::endl;
-    wi->makeSound(); //will output the wrong_cat sound!
-    wmeta->makeSound();
-
-    delete wmeta;
-    delete wi;
-
+        delete wmeta;
+        delete wi;
+    }
     return 0;
 }
--- cpp/cpp04/ex01/main.cpp
+++ cpp/cpp04/ex01/main.cpp
@@ -2,45 +2,51 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
 
-int main()
+int main(int ac, char **av)
 {
-    std::cout << "<basic test>" << std::endl;
+    if (ac != 2)
+        return -1;
 
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
-    delete j;//should not create a leak
-    delete i;
-
-    std::cout << "------------------------------------------------" << std::endl;
-
-    std::cout << "<deep copy test>" << std::endl;
-    Cat* c1 = new Cat();
-    c1->getBrain()->addIdeas("i am cat");
-    
-    Cat* c2 = new Cat(*c1);
-    c1->getBrain()->addIdeas("give me fish");
-
-    std::cout << std::endl << "c1 brain" << std::endl;
-    std::cout << c1->getBrain()->getIdeas() << " " << std::endl;
-    std::cout << "c2 brain" << std::endl;
-    std::cout << c2->getBrain()->getIdeas() << " " << std::endl;
-    delete c1;
-    delete c2;
-
-    std::cout << "------------------------------------------------" << std::endl;
-
-    std::cout << "<subtype polymorphism test>" << std::endl;
-    Animal *arr[2];
-    for (int i = 0; i < 2; i++)
+    if (std::string(av[1]) == "1")
     {
-        if (i % 2 == 0)
-            arr[i] = new Cat();
-        else
-            arr[i] = new Dog();
+        std::cout << "<basic test>" << std::endl;
+
+        const Animal* j = new Dog();
+        const Animal* i = new Cat();
+        delete j;//should not create a leak
+        delete i;
     }
-    arr[0]->makeSound();
-    arr[1]->makeSound();
-    for (int i = 0; i < 2; i++)
-        delete arr[i];        
+    else if (std::string(av[1]) == "2")
+    {
+        std::cout << "<deep copy test>" << std::endl;
+        Cat* c1 = new Cat();
+        c1->getBrain()->addIdeas("i am cat");
+        
+        Cat* c2 = new Cat(*c1);
+        c1->getBrain()->addIdeas("give me fish");
+
+        std::cout << std::endl << "c1 brain" << std::endl;
+        std::cout << c1->getBrain()->getIdeas() << " " << std::endl;
+        std::cout << "c2 brain" << std::endl;
+        std::cout << c2->getBrain()->getIdeas() << " " << std::endl;
+        delete c1;
+        delete c2;
+    }
+    else
+    {
+        std::cout << "<subtype polymorphism test>" << std::endl;
+        Animal *arr[2];
+        for (int i = 0; i < 2; i++)
+        {
+            if (i % 2 == 0)
+                arr[i] = new Cat();
+            else
+                arr[i] = new Dog();
+        }
+        arr[0]->makeSound();
+        arr[1]->makeSound();
+        for (int i = 0; i < 2; i++)
+            delete arr[i];        
+    }
     return 0;
 }
