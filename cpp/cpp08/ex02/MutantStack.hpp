b7679385f4f24fc817b901d4cfd7e74699e6617d
minseunk <minseunk@c2r1s6.42seoul.kr>
2024-04-25 16:43:26 +0900

    08_copy_const

--- cpp/cpp08/ex02/MutantStack.hpp
+++ cpp/cpp08/ex02/MutantStack.hpp
@@ -10,16 +10,16 @@ class MutantStack : public std::stack<T>
     public:
         MutantStack() {};
         ~MutantStack() {};
-        MutantStack(MutantStack& ref) 
+        MutantStack(MutantStack const & ref) 
         {    
-            for (MutantStack<T>::iterator it = ref.begin(); it != ref.end(); it++)
+            for (MutantStack<T>::const_iterator it = ref.begin(); it != ref.end(); it++)
                 (*this).push(*it);
         };
-        MutantStack& operator=(MutantStack& ref) 
+        MutantStack& operator=(MutantStack const & ref) 
         {
             if (this != &ref)
             {
-                for (MutantStack<T>::iterator it = ref.begin(); it != ref.end(); it++)
+                for (MutantStack<T>::const_iterator it = ref.begin(); it != ref.end(); it++)
                     (*this).push(*it);
             }
             return *this;
@@ -27,9 +27,15 @@ class MutantStack : public std::stack<T>
         typedef typename MutantStack<T>::stack::container_type::iterator iterator;
         iterator begin() {return this->c.begin();}
         iterator end() {return this->c.end();}
+        typedef typename MutantStack<T>::stack::container_type::const_iterator const_iterator;
+        const_iterator begin() const { return this->c.begin(); }
+        const_iterator end() const { return this->c.end(); }
         typedef typename MutantStack<T>::stack::container_type::reverse_iterator reverse_iterator;
         reverse_iterator rbegin() {return this->c.rbegin();}
         reverse_iterator rend() {return this->c.rend();}
+        typedef typename MutantStack<T>::stack::container_type::const_reverse_iterator const_reverse_iterator;
+        const_reverse_iterator rbegin() const {return this->c.rbegin();}
+        const_reverse_iterator rend() const {return this->c.rend();}
 };
 
 #endif
