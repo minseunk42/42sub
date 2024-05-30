34c33df868f600209c6bfcd58b7c68cc25fffeb9
minseunk <minseunk@c2r1s4.42seoul.kr>
2024-05-30 11:16:18 +0900

    0902_time

--- cpp/cpp09/ex02/PmergeMe.cpp
+++ cpp/cpp09/ex02/PmergeMe.cpp
@@ -28,8 +28,20 @@ void PmergeMe::sort()
         out += " ";
     }
     std::cout << out << std::endl;
-    
-    // sort with count;
+    struct timespec start, end;
+
+    // 시작 시간 측정
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    // 측정하고자 하는 작업
+    for (int i = 0; i < 100000000; ++i);
+
+    // 끝 시간 측정
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    // 경과 시간 계산
+    double elapsed = ((end.tv_sec - start.tv_sec) / 1e12) + (end.tv_nsec - start.tv_nsec);
+    double elapsed2 = (end.tv_nsec - start.tv_nsec);
 
     out = "After  : ";
     for (std::list<unsigned int>::iterator it = l.begin(); it != l.end(); ++it)
@@ -39,6 +51,6 @@ void PmergeMe::sort()
         out += ss.str();
         out += " ";
     }
-    std::cout << out << std::endl;
-    std::cout << cnt << std::endl; 
+    std::cout << "Time to process a range of 3000 elements with std::vector : " << elapsed / 1000000000 << "us" << std::endl;
+    std::cout << "Time to process a range of 3000 elements with std::list   : " << elapsed2 / 1000000 << "us" << std::endl;
 }
